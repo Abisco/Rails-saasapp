b1ddee638fa9bdb30b57569e8773b14a78cd2e2e
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2017-03-17 03:11:46 -0400

    Added mailtrap support

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app65129876@heroku.com',
+  :password => 'ua7jfbt33086',
+  :domain => 'rails-saasapp.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
